469b27904ff08ab094935c279d2abd3eb12dd4dc
Gustavo Mourato <gmam@cesar.school>
2025-10-23 17:01:51 -0300

    adding graphs without connetions and updating requirements.txt

---
+++ src/graphs/io.py
@@ -0,0 +1,35 @@
+from pyvis.network import Network
+import os
+
+bairros = [
+    "Aflitos", "Afogados", "Agua Fria", "Alto Jose Bonifacio", "Alto Jose do Pinho",
+    "Alto Santa Teresinha", "Alto do Mandu", "Apipucos", "Areias", "Arruda",
+    "Barro", "Beberibe", "Boa Viagem", "Boa Vista", "Bomba do Hemeterio",
+    "Bongi", "Brasilia Teimosa", "Brejo da Guabiraba", "Brejo de Beberibe",
+    "Cabanga", "Cacote", "Cajueiro", "Campina do Barreto", "Campo Grande",
+    "Casa Amarela", "Casa Forte", "Caxanga", "Cidade Universitaria", "Coelhos",
+    "Cohab", "Coqueiral", "Cordeiro", "Corrego do Jenipapo", "Curado", "Derby",
+    "Dois Irmaos", "Dois Unidos", "Encruzilhada", "Engenho do Meio", "Espinheiro",
+    "Estancia", "Fundao", "Gracas", "Guabiraba", "Hipodromo", "Ibura",
+    "Ilha Joana Bezerra", "Ilha do Leite", "Ilha do Retiro", "Imbiribeira",
+    "Ipsep", "Iputinga", "Jaqueira", "Jardim Sao Paulo", "Jiquia", "Jordao",
+    "Linha do Tiro", "Macaxeira", "Madalena", "Mangabeira", "Mangueira",
+    "Monteiro", "Morro da Conceicao", "Mustardinha", "Nova Descoberta", "Paissandu",
+    "Parnamirim", "Passarinho", "Pau-Ferro", "Peixinhos", "Pina", "Poco",
+    "Ponto de Parada", "Porto da Madeira", "Prado", "Recife", "Rosarinho",
+    "San Martin", "Sancho", "Santana", "Santo Amaro", "Santo Antonio", "Sao Jose",
+    "Sitio dos Pintos", "Soledade", "Tamarineira", "Tejipio", "Torre", "Torreao",
+    "Torroes", "Toto", "Varzea", "Vasco da Gama", "Zumbi"
+]
+
+# Cria a rede
+net = Network(height="750px", width="100%", bgcolor="#ffffff", font_color="black")
+
+# Adiciona os bairros como nós
+for bairro in bairros:
+    net.add_node(bairro, label=bairro)
+
+# Salva o arquivo HTML na pasta 'out' (voltando um nível de diretório)
+net.write_html("../../out/grafo_bairros.html")
+
+print("✅ Grafo criado com sucesso! Abra o arquivo 'out/grafo_bairros.html' no navegador.")
