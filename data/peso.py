1290596ef42b60f1855a22b4ebd770fc3e520edf
Gustavo Mourato <gmam@cesar.school>
2025-11-08 12:07:19 -0300

    terminando conexoes entre bairros e adicionando peso nas arestas

---
+++ data/peso.py
@@ -0,0 +1,53 @@
+import csv
+
+def atribuir_peso(logradouro):
+    if not isinstance(logradouro, str):
+        return ""
+    
+    logradouro_limpo = logradouro.strip()
+    
+    if logradouro_limpo.startswith("R."):
+        return 1
+    elif logradouro_limpo.startswith("Estr."):
+        return 1
+    elif logradouro_limpo.startswith("Pte."):
+        return 2
+    elif logradouro_limpo.startswith("Psa."):
+        return 2
+    elif logradouro_limpo.startswith("Av."):
+        return 3
+    elif logradouro_limpo.startswith("Rod. "):
+        return 3
+    else:
+        return ""
+
+arquivo_entrada = 'data\Conexões - adjacencias_bairros.csv'
+arquivo_saida = 'data\conexoes_com_peso.csv'
+
+with open(arquivo_entrada, mode='r', encoding='utf-8') as f_entrada:
+    leitor_csv = csv.DictReader(f_entrada)
+    
+    colunas = leitor_csv.fieldnames
+    
+    if 'peso' not in colunas:
+        colunas.append('peso')
+        
+    linhas_atualizadas = []
+
+    for linha in leitor_csv:
+        logradouro_atual = linha.get('logradouro', '')
+        
+        novo_peso = atribuir_peso(logradouro_atual)
+        
+        linha['peso'] = novo_peso
+        
+        linhas_atualizadas.append(linha)
+
+with open(arquivo_saida, mode='w', encoding='utf-8', newline='') as f_saida:
+    escritor_csv = csv.DictWriter(f_saida, fieldnames=colunas)
+    
+    escritor_csv.writeheader()
+    
+    escritor_csv.writerows(linhas_atualizadas)
+
+print(f"Arquivo '{arquivo_saida}' criado")
